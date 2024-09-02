5460cbfbfcc72e1d2dc60d9208a3d6626a87e146
Danilo Ponciano <danilo_ponciano@estudante.sc.senai@gmail.com>
2024-09-02 20:03:25 -0300

    first commit of project back-end

---
+++ src/test/java/crio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package crio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
